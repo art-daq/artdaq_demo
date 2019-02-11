1cbdce450636e8bc639ef4280e5ded5fe3d0d199
Eric Flumerfelt <eflumerf@fnal.gov>
2019-02-11 09:55:42 -0600

    Use run_demo from source repo, and make sure it has its execute bit set. #21834

--- tools/quick-mrb-start.sh
+++ tools/quick-mrb-start.sh
@@ -463,7 +463,8 @@ sed -i -r 's!^\s*data_directory_override.*!data_directory_override: '$datadir'!'
 sed -i -r 's!^\s*DAQ setup script:.*!DAQ setup script: '$Base'/setupARTDAQDEMO!' boot.txt
 
 cd $Base
-wget https://cdcvs.fnal.gov/redmine/projects/artdaq-demo/repository/revisions/develop/raw/tools/run_demo.sh
+cp $MRB_SOURCE/artdaq_demo/tools/run_demo.sh .
+chmod +x run_demo.sh
 
 if [ "x${opt_run_demo-}" != "x" ]; then
 	echo doing the demo
