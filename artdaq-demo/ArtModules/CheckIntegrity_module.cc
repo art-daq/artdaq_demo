69eadd487c01bbab26068a3370aa31b1f208b992
Eric Flumerfelt <eflumerf@fnal.gov>
2019-10-17 11:52:46 -0500

    Change TRACE level in CheckIntegrity module to quiet it down during normal running

--- artdaq-demo/ArtModules/CheckIntegrity_module.cc
+++ artdaq-demo/ArtModules/CheckIntegrity_module.cc
@@ -96,7 +96,7 @@ void demo::CheckIntegrity::analyze(art::Event const& evt)
 		}
 	}
 
-	TLOG(TLVL_INFO) << "Run " << evt.run() << ", subrun " << evt.subRun() << ", event " << evt.event() << " has "
+	TLOG(TLVL_DEBUG) << "Run " << evt.run() << ", subrun " << evt.subRun() << ", event " << evt.event() << " has "
 	                << fragments.size() << " fragment(s) of type TOY1 or TOY2";
 
 	bool err = false;
